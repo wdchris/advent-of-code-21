0ad7bcfae5db72538edc08de30516fbe338d27bd
Chris Harding <chris.harding@cazoo.co.uk>
2021-12-22 14:45:55 +0000

    add daily function caller

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -15,4 +15,15 @@ defmodule AdventOfCode do
   def hello do
     :world
   end
+
+  def day_one(part) do
+    case part do
+    1 ->
+      DayOne.read_file("resources/day_one_input.dat")
+      |> DayOne.sonar_sweep()
+    2 ->
+      DayOne.read_file("resources/day_one_input.dat")
+      |> DayOne.sonar_sweep_sliding()
+    end
+  end
 end
